eeaeda0147c0765b2f323c766ad1000dad07b4a2
Alexandre Morlat <alexandre-morlat_student2021@wilder.school>
2021-04-06 11:53:34 +0200

    Solution

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require_once 'Bicycle.php';
 require_once 'Car.php';
 require_once 'Truck.php';
+require_once 'MotorWay.php';
+require_once 'PedestrianWay.php';
+require_once 'ResidentialWay.php';
 
 $flanders = new Car('Purple', 4, 'gasoline');
 $homer = new Car('Pink', 5, 'gasoline');
@@ -41,3 +44,15 @@ $truck2->forward();
 var_dump($truck2->getCurrentSpeed());
 $truck2->brake();
 var_dump($truck2->getCurrentSpeed());
+
+$motorWay=new MotorWay(4,130);
+$pedestrianWay= new PedestrianWay(1,10);
+$residentialWay= new ResidentialWay(2,50);
+
+$motorWay->addVehicle($bike);
+$pedestrianWay->addVehicle($bike);
+$residentialWay->addVehicle($marge);
+
+print_r($motorWay);
+print_r($pedestrianWay);
+print_r($residentialWay);
